3a95137cc4ea2293b10fb16e50b8900b8d2054e5
laurindolucas <caiolucax2024@gmail.com>
2025-09-08 13:30:01 -0300

    tradução da página de aboutUs e também a criação do código que faz o contato funcionar

---
+++ form.js
@@ -0,0 +1,17 @@
+// Inicializa EmailJS com sua Public Key
+emailjs.init("o4aP_GKwosXjPlQ4q"); 
+
+// Captura o envio do formulário
+document.getElementById("meuForm").addEventListener("submit", function(e){
+  e.preventDefault(); 
+
+  emailjs.sendForm("service_zgkv3sn", "template_p5f7qmj", this)
+    .then(() => {
+      alert(" Nachricht erfolgreich gesendet!");
+      this.reset(); 
+    })
+    .catch((err) => {
+      console.error(" Fehler:", err);
+      alert("Fehler beim Senden, bitte erneut versuchen.");
+    });
+});
